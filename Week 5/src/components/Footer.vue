eaa9c03ba4b884b330e42b9145c31b2cb70bf2a3
UberNomad <49048741+UberNomad@users.noreply.github.com>
2021-09-15 23:03:41 +1000

    Add files via upload

---
+++ Week 5/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <span>Copyright {{year}} | Website by {{author}}</span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    author: String,
+    year: String
+  }
+}
+</script>
+
+<style scoped>
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+	text-align: center;
+  padding: 5px 0;
+}
+</style>
---
+++ Week 5/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+    <h1>Week 5</h1>
+    <h2>Testing v-else</h2>
+    <p v-if="test1">Test 1: True</p>
+    <p v-else>Test 1: False</p>
+    <h2>Testing v-if</h2>
+    <p v-if="test2">Test 2: True</p>
+    <p v-else>Test 2: False</p>
+    <h2>To-do List</h2>
+    <ol>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }}
+      </li>
+    </ol>
+    <label>Message: </label><input v-model="message">
+    <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  data: function() {
+    return {
+      test1: false,
+      test2: true,
+      todos: [
+          { text: 'Learn JavaScript' },
+          { text: 'Learn Vue' },
+          { text: 'Build something awesome' }
+        ],
+        message: 'Hello Vue.js!',
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
